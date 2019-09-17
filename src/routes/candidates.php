bcef0dd8e150a677266c91728b0f2227d0621f38
nasona <nasona20@mail.wlu.edu>
2019-09-17 09:45:24 -0400

    added new routes for precincts, states, counties, etc.

--- src/routes/candidates.php
+++ src/routes/candidates.php
@@ -33,4 +33,52 @@ $app -> get('/candidates/{name}', function(Request $request, Response $response)
     $conn->close();
 });
 
+# Route to get all counties in a state
+$app -> get('/counties-by-state/{state_name}', function(Request $request, Response $response) {
+    $conn = mysqli_connect('localhost', 'mockconv_testdbc', 'dbtest06984527', 'mockconv_database');
+    $response = $conn->query('SELECT c.county_name, c.median_income, c.avg_education,c.last_election_party FROM County c,State s WHERE c.state_ID = s.state_id and state_name='."'$state_name'");
+    if($response) {
+        return get($response);
+    } else {
+        return 'Oops, server got in trouble.';
+    };
+    $conn->close();
+});
+
+# Route to precincts by county
+$app -> get('/precincts-by-county/{county_name}', function(Request $request, Response $response) {
+    $conn = mysqli_connect('localhost', 'mockconv_testdbc', 'dbtest06984527', 'mockconv_database');
+    $response = $conn->query('SELECT c.county_name, p.precinct_name FROM County c,Precinct p WHERE c.county_ID = p.county_id and county_name='."'$county_name'");
+    if($response) {
+        return get($response);
+    } else {
+        return 'Oops, server got in trouble.';
+    };
+    $conn->close();
+});
+
+# Route to state elections
+$app -> get('/state-election-result/{state_name}', function(Request $request, Response $response) {
+    $conn = mysqli_connect('localhost', 'mockconv_testdbc', 'dbtest06984527', 'mockconv_database');
+    $response = $conn->query('SELECT s.state_name, e.election_name, e.year, e.type, e.winner FROM State s, State_Election se, Election e WHERE s.state_ID = se.state_id and se.election_id = e.election_id and state_name='."'$state_name'");
+    if($response) {
+        return get($response);
+    } else {
+        return 'Oops, server got in trouble.';
+    };
+    $conn->close();
+});
+
+# Route to county elections
+$app -> get('/county-election-result/{county_name}', function(Request $request, Response $response) {
+    $conn = mysqli_connect('localhost', 'mockconv_testdbc', 'dbtest06984527', 'mockconv_database');
+    $response = $conn->query('SELECT c.county_name, e.election_name, e.year, e.type, e.winner FROM County c, County_Election ce, Election e WHERE c.county_ID = ce.county_id and ce.election_id = e.election_id and county_name='."'$county_name'");
+    if($response) {
+        return get($response);
+    } else {
+        return 'Oops, server got in trouble.';
+    };
+    $conn->close();
+});
+
 ?>
